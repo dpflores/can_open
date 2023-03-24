75a8c881639599d416b5b38277d5d4da1b6fab63
Del Piero Flores <delpier22.flores@gmail.com>
2023-03-24 18:04:47 -0500

    U

--- test.py
+++ test.py
@@ -18,14 +18,30 @@
 
 # import the library
 
-from socket_can import *
+import socket
+import struct
 
-# CAN configuration
-can_id = 0x10
-can_frame_fmt = "=IB3x8s"
-can_port = "can1"
+can_frame_fmt = "=IB3x8s";
+can_frame_size = struct.calcsize (can_frame_fmt)
 
-can = CAN(can_frame_fmt, can_port, can_id)
+def dissect_can_frame(frame):
+    can_id, can_dlc, data = struct.unpack(can_frame_fmt, frame) 
+    return (can_id, can_dlc, data[:can_dlc])
+
+s = socket.socket (socket.AF_CAN, socket.SOCK_RAW, socket.CAN_RAW)
+s.bind(("can0",))
 
 while True:
-    print(can.get_vel(sender_id=0xA5))
+    cf, addr = s.recvfrom(16)
+    can_id = dissect_can_frame(cf)[0]
+    if can_id == 165 :
+        can_dlc = dissect_can_frame(cf)[1]
+        dataL = hex((dissect_can_frame(cf)[2])[0])#Data LSB
+        dataH = hex((dissect_can_frame(cf)[2])[1])#Data HSB
+   
+        #data= (int (dataH,16))<<8| int (dataL,16)
+        data = int (dataH,16) + int (dataL,16)
+
+        #print (dataL)
+        #print(dataH )
+        print (data)
