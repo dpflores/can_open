8a81ce89f6a547101473af324125a9265e94f61d
Del Piero Flores <delpier22.flores@gmail.com>
2023-03-24 15:59:23 -0500

    u

--- test.py
+++ test.py
@@ -15,3 +15,10 @@ from can_open import *
 import canopen
 
 network = canopen.Network()
+
+# This will attempt to read an SDO from nodes 1 - 127
+network.scanner.search()
+# We may need to wait a short while here to allow all nodes to respond
+time.sleep(0.05)
+for node_id in network.scanner.nodes:
+    print("Found node %d!" % node_id)
