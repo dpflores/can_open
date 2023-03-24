70a043bbdbd4abf2e7e7b7ff57e380ed830b6980
Del Piero Flores <delpier22.flores@gmail.com>
2023-03-24 17:33:40 -0500

    u

--- test.py
+++ test.py
@@ -2,15 +2,19 @@ from can_open import *
 
 
 #CAN
-port = 'can0'
+port = 'can1'
 id = 10
 can_open = CANOPEN(port, id)    
 
 
-while True:
-    data = can_open.get_raw_data(0x3403)
-    print(f"data: {data}")
+# Para el JD2120
+# while True:
+#     data = can_open.get_raw_data(0x3403)
+#     print(f"data: {data}")
 
+while True:
+    data = can_open.get_raw_data(0xA5)
+    print(f"data: {data}")
 
 
 
