0c2874ee4ac64de50c486af37922b881e2b749cd
Del Piero Flores <delpier22.flores@gmail.com>
2023-03-24 15:58:29 -0500

    u

---
+++ test.py
@@ -0,0 +1,17 @@
+from can_open import *
+
+
+# #CAN
+# port = 'can0'
+# id = 165
+# can_open = CANOPEN(port, id)    
+
+
+# while True:
+#     speed = can_jd.get_speed_stimation()
+#     print(f"velocidad: {speed} m/s")
+
+
+import canopen
+
+network = canopen.Network()
